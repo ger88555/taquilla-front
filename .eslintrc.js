f77f6ea38aa4c6f5a806c2c140554db51bbd5ac1
ger88555 <germanadolf@protonmail.com>
2021-12-21 03:36:38 -0700

    Setup React, ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jest/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 13,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'jest'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
