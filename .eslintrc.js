ea792e0707ad0557b2bdb2acab389bee5de9c1d4
ger88555 <germanadolf@protonmail.com>
2021-12-21 03:43:47 -0700

    Updated .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,11 @@ module.exports = {
         'react',
         'jest'
     ],
+    'settings': {
+        'react': {
+            'version': 'detect'
+        }
+    },
     'rules': {
         'indent': [
             'error',
