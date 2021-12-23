86a4799758e43412fab661894cf3373d8718da54
Jordan Ibarra <bxordan@gmail.com>
2021-12-23 15:49:30 -0700

    added bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+//import App from './App'
 import reportWebVitals from './reportWebVitals'
+import LoginForm from './LoginForm'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <LoginForm />
     </React.StrictMode>,
     document.getElementById('root')
 )
