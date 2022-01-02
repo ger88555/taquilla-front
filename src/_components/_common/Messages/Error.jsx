d8a2a9861a5275a8167139707c6bbd8b6c27e581
ger88555 <germanadolf@protonmail.com>
2022-01-01 21:57:24 -0700

    style: tweak error message interface

--- src/_components/_common/Messages/Error.jsx
+++ src/_components/_common/Messages/Error.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Alert, Button } from 'react-bootstrap'
+import { Alert, Button, Image } from 'react-bootstrap'
+import empty from '../../../_assets/empty.svg'
+import retry from '../../../_assets/retry.svg'
 
 export const Error = ({ onReload = null, children }) => (
-    <Alert variant='danger' className='lead'>
-        <strong className='font-weight-bold'>Error: </strong>{children}
+    <Alert variant='danger' className='lead' style={{ fontSize: '2em' }}>
+        <strong>No se pudo conseguir la cartelera.</strong>
         <br/>
-        {onReload && <Button onClick={onReload} size='lg' className='mt-3' data-testid="reload-button" >Reintentar</Button>}
+        <Image src={empty} style={{ opacity: '50%', maxHeight: '40vh' }} />
+        <br/>
+        <strong>Error: </strong>{children}
+        <br/>
+        {onReload && <Button onClick={onReload} size='lg' className='mt-3' data-testid="reload-button">
+            <Image src={retry} /> Reintentar
+        </Button>}
     </Alert>
 )
