4dea5e31703d614cd51ab1bd78b245bfdf9557e4
ger88555 <germanadolf@protonmail.com>
2022-01-03 10:26:46 -0700

    fix: read `HASH_ROUTES` env variable correctly

--- src/_components/_common/Router/RouterType.jsx
+++ src/_components/_common/Router/RouterType.jsx
@@ -3,7 +3,9 @@ import {
     HashRouter,
 } from 'react-router-dom'
 
+const shouldHash = JSON.parse(process.env.REACT_APP_HASH_ROUTES || false)
+
 /**
  * Get the router type that fits the App configuration
  */
-export const RouterType = process.env.REACT_APP_HASH_ROUTES ? HashRouter : BrowserRouter
+export const RouterType = shouldHash ? HashRouter : BrowserRouter
