74cce297b2d3516c04443f23742cb571c7693def
Jordan Ibarra <bxordan@gmail.com>
2022-01-02 20:59:29 -0700

    provisional router

--- src/_components/_layouts/App/App.jsx
+++ src/_components/_layouts/App/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './App.css'
-// import { Login } from '../../_views/Login'
+import { Login } from '../../_views/Login'
 import { Billboard } from '../../_views/Billboard'
-
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    // Link
+} from 'react-router-dom'
 
 export const App = () => {
     return (
         <div className="App">
-            {/* <Login /> */}
-            <Billboard />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login />} />
+                    <Route path='/cartelera' element={<Billboard />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
