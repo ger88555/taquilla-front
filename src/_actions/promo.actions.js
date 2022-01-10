5ed9fb52a013442d64b71104f72cf3271c5e4b9f
Jordan Ibarra <bxordan@gmail.com>
2022-01-10 09:01:55 -0700

    adding addPromo actions

--- src/_actions/promo.actions.js
+++ src/_actions/promo.actions.js
@@ -28,4 +28,21 @@ function list(params = {}){
     function success(data) { return { type: promoConstants.PROMOS_SUCCESS, payload: data } }
     function failure(error) { return { type: promoConstants.PROMOS_FAILURE, payload: error } }    
 
-}        
+}
+
+function addPromo(fields){
+    dispatch(request())
+
+    promoService.addPromo(fields)
+        .then(
+            data => {
+                dispatch(success(data))                    
+            },
+            error => {
+                dispatch(failure(error))
+            }
+        )
+    function request() { return { type: promoConstants.PROMOS_ADD_REQUEST } }
+    function success(data) { return { type: promoConstants.PROMOS_ADD_SUCCESS, payload: data } }
+    function failure(error) { return { type: promoConstants.PROMOS_ADD_FAILURE, payload: error } }
+}
