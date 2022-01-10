0c1e78118e015858a82d724e60e1a4d2e0004b63
Jordan Ibarra <bxordan@gmail.com>
2022-01-10 09:03:49 -0700

    adding pay to cart service

--- src/_services/cart.service.js
+++ src/_services/cart.service.js
@@ -21,3 +21,20 @@ function get(id) {
             Promise.reject(err.data?.message || 'Hubo un error al solicitar los datos del pedido.')
         ))
 }
+
+function pay(id){
+    return axios
+    .put(`/carts/${id}/pay`)
+    .then(res => {
+        const { data } = res
+
+        if (data.success === false) {
+            return Promise.reject(res)
+        }
+
+        return data.data
+    })
+    .catch(err => (
+        Promise.reject(err.data?.message || 'Hubo un error al solicitar los datos del pedido.')
+    ))
+}
