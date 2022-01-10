6ae61d8edcea67f2d9b42af9864c7e4d25570d08
Jordan Ibarra <bxordan@gmail.com>
2022-01-09 19:23:25 -0700

    Added Exhibitions view

---
+++ src/_components/_views/Exhibitions/Exhibitions.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ExhibitionsTable } from '../../_layouts/App/ExhibitionsTable'
+
+export const Exhibitions = () => {
+    return (
+        <ExhibitionsTable />
+    )
+}
