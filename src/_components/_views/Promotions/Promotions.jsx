714ae71a42ca2fce875f91e075ab101343963042
Jordan Ibarra <bxordan@gmail.com>
2022-01-10 18:06:18 -0700

    adding promotions

---
+++ src/_components/_views/Promotions/Promotions.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { PromotionsTable } from '../../_layouts/App/PromotionsTable'
+
+export const Promotions = () => {
+    return (
+        <PromotionsTable />
+    )
+}
